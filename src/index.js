e03cdf50d5f906d81ab7e6816f6da942f3c9db6e
Irene Martinez <imartinez921@gmail.com>
2022-07-25 00:49:24 -0400

    Add Letters1 section outline

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-// import Example from './scripts/example';
+import Section from './scripts/section';
+import Letter from './scripts/letter';  
 
 
-
-// ctx.fillStyle = '#800080'
-// ctx.fillRect(0,0,900,500);
 
 document.addEventListener('DOMContentLoaded', () => {
+
     console.log("hello world")
 
     const myCanvas = document.getElementById('mycanvas');
-    myCanvas.width = 100;
-    myCanvas.height = 571006;
+    myCanvas.width = 1000;
+    myCanvas.height = 1000;
     
     const ctx = myCanvas.getContext('2d')
 
@@ -20,7 +19,18 @@ document.addEventListener('DOMContentLoaded', () => {
         myCanvas.width = 1024;
         myCanvas.height = 700;
         ctx.drawImage(background,0,0,background.width,background.height,0,0,1024,700);
-    }
-
+        console.log('I am the image')
+        
+        const letters1 = new Section (ctx, 100,0,300,500);
+        const letters2 = new Section (ctx, 0,0,300,500);
+    } 
 
 })
+    
+
+
+    function animate() { 
+        window.requestAnimationFrame(animate)
+        console.log('am I animating')
+        c.clearRect(0,0,myCanvas.width, myCanvas.height)
+    }
